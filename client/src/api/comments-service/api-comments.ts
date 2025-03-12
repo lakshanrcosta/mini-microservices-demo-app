8d05665ff0be0fab709d64c20ec2126774cf2851
Lakshan Costa <lakshanrcosta@gmail.com>
2025-03-12 17:31:03 +0200

    Add API functions to create and retrieve comments and posts

--- client/src/api/comments-service/api-comments.ts
+++ client/src/api/comments-service/api-comments.ts
@@ -1,5 +1,14 @@
+import { CommentDto } from '../../types/comment-types/comment-dto-types';
 import { createCommentsApiInstance } from './api-config-comments';
 
 const apiComments = createCommentsApiInstance();
+
+export const createComment = (postId: string, dto: CommentDto) => {
+  return apiComments.post(`/posts/${postId}/comments`, dto);
+};
+
+export const getComments = (postId: string) => {
+  return apiComments.get(`/posts/${postId}/comments`);
+};
 
 export default apiComments;
