ba002abac9ebc66b9b435dad96e938b07e2361ef
Lakshan Costa <lakshanrcosta@gmail.com>
2025-03-16 11:52:48 +0200

    Initialize event broker service with Express, middleware, and server setup

---
+++ event-broker/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import router from './routes/routes';
+import { loggingMiddleware } from './middlewares/loggingMiddleware';
+import { authMiddleware } from './middlewares/authMiddleware';
+import { errorMiddleware } from './middlewares/errorMiddleware';
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// Custom middleware
+app.use(loggingMiddleware);
+// app.use(authMiddleware);
+
+// Routes
+app.use('/api', router);
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+const server = http.createServer(app);
+
+// Start the server
+const port = process.env.PORT || 3305;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
